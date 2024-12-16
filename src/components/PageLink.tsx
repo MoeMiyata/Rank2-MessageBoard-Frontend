a8e1ce46b0621c0a1718878eab2b3720eab28ba4
MIYATA Moe <moe.miyata.1025@gmail.com>
2024-12-16 10:44:16 +0900

    前へボタン 追加

--- src/components/PageLink.tsx
+++ src/components/PageLink.tsx
@@ -1,10 +1,23 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { PageLinkContext } from '../providers/PageLinkProvider.tsx';
+import { UserContext } from '../providers/UserProvider.tsx';
+import { getList } from '../api/Post.tsx';
 
 export default function PageLink() {
+    const { userInfo } = useContext(UserContext);
     const { pageNumber } = useContext(PageLinkContext);
     const { setPageNumber } = useContext(PageLinkContext);
+
+    let IsExistNextPage = false;
+
+    const judgeOfNextPage = async () => {
+        const posts = await getList(userInfo.token, pageNumber);
+        if (posts.length > 0) {
+            IsExistNextPage = true;
+        }
+        // return IsExistNextPage;
+    }
 
     const onBeforePageClick = async () => {
         console.log('before');
@@ -12,17 +25,20 @@ export default function PageLink() {
     }
 
     const onNextPageClick = async () => {
-      console.log('next');
-      await setPageNumber((prevPage) => prevPage += 10);
+        console.log('next');
+        await setPageNumber((prevPage) => prevPage += 10);
     }
 
     console.log('pageNumber:', pageNumber);
 
+    judgeOfNextPage();
+
+    
 	return (
         <SPageLink>
             <SPageLinkRow>
-                {!(pageNumber<10) ? <SPageLinkButton onClick={onBeforePageClick}>前へ</SPageLinkButton> : null}
-                <SPageLinkButton onClick={onNextPageClick}>次へ</SPageLinkButton>
+                { !(pageNumber<10) ? <SPageLinkBeforeButton onClick={onBeforePageClick}>前へ</SPageLinkBeforeButton> : null }
+                { IsExistNextPage ? <SPageLinkNextButton onClick={onNextPageClick}>次へ</SPageLinkNextButton> : null }
             </SPageLinkRow>
         </SPageLink>
 	)
@@ -31,6 +47,8 @@ export default function PageLink() {
 
 const SPageLink = styled.div`
   padding: 8px;
+  height: 100%; 
+  overflow-y: scroll;
 `
 
 const SPageLinkRow = styled.div`
@@ -39,7 +57,17 @@ const SPageLinkRow = styled.div`
   text-align: rigth;
 `
 
-const SPageLinkButton = styled.button`
+const SPageLinkBeforeButton = styled.button`
+  
+  background-color: #222222;
+  padding: 4px;
+  border-radius: 8px;
+  color: #FAFAFA;
+  width: 100%;
+  width: 15%;
+`
+
+const SPageLinkNextButton = styled.button`
   background-color: #222222;
   padding: 4px;
   border-radius: 8px;
