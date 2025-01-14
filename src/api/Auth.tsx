644c18d9c44ffb85cfe27f924652ce499d73b35a
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-14 15:04:31 +0900

    database期限切れ更新

--- src/api/Auth.tsx
+++ src/api/Auth.tsx
@@ -5,6 +5,8 @@ export const sign_in = async (user_id: string, pass: string) => {
   // const url = `http://localhost:3000/auth?user_id=${user_id}&pass=${pass}`;
   const url = hostUrl + `/auth?user_id=${user_id}&pass=${pass}`;
   console.log(url);
+  console.log(hostUrl);
+  
 
   const res = await axios.get(url);
   console.log(res);
