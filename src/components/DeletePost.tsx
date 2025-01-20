ccbf1c63e541f5ebbc405b87392dc94c08a23efa
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-20 13:21:26 +0900

    メッセージの削除

--- src/components/DeletePost.tsx
+++ src/components/DeletePost.tsx
@@ -1,45 +1,24 @@
 import React, { useContext } from "react"
 import styled from "styled-components"
 
-import { PostListContext, PostType } from "../providers/PostListProvider.tsx";
 import { UserContext } from "../providers/UserProvider.tsx";
-import { PageLinkContext } from "../providers/PageLinkProvider.tsx";
 import { deletePost, getList } from "../api/Post.tsx";
+import { ReloadPage } from "./ReloadPage.tsx";
 
 
 export const DeletePost = (props: any) => {
     const { deleteid } = props;
 
     const { userInfo } = useContext(UserContext);
-    const { setPostList } = useContext(PostListContext);
-    const { pageNumber } = useContext(PageLinkContext);
 
-    // ポスト一覧を取得して、対象のポストのみ削除する関数
-    const getPostListExcludeOne = async() => {
-        const posts = await getList(userInfo.token, pageNumber);
-        // getListで取得したポスト配列をコンテキストに保存する
-        let deleteRecord: PostType | null = null;
-        if (posts) {
-            posts.forEach((p: any) => {
-                console.log("p.id:", p.id, "deleteid:", deleteid);
-                if (p.id === deleteid) { // 取得したポストが削除したいポストidと一致しなければ追加
-                    console.log("p.id:", p.id);
-                    deleteRecord = {
-                        id: p.id,
-                        user_name: p.user_name,
-                        content: p.content,
-                        created_at: new Date(p.created_at),
-                    };
-                }
-            });
-        }
-        console.log("record(getPostListExcludeOne):", deleteRecord);
-        const deletesPosts = await deletePost(userInfo.token, deleteid);
-        console.log("deletesPosts(getPostListExcludeOne):", deletesPosts);
+    // 対象のポストのみ削除してリロードする関数
+    const deletePostReload = async() => {
+        await deletePost(userInfo.token, deleteid);
+        ReloadPage()
     }
 
     const onDeletePostClick = () => {
-        getPostListExcludeOne()
+        deletePostReload()
         console.log("post.id:", deleteid, "のメッセージ削除");
     }
 
