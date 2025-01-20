82d5ad2739b52f5bd674c9a0dcf801842a4e5f2c
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-20 17:56:26 +0900

    メッセージ検索（検索機能の実装）

--- src/components/SearchPost.tsx
+++ src/components/SearchPost.tsx
@@ -1,17 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
+import { UserContext } from "../providers/UserProvider";
+import { PostListContext, PostType } from "../providers/PostListProvider";
+import { PageLinkContext } from "../providers/PageLinkProvider";
+import { getList } from "../api/Post";
 
 export const SearchPost = () => {
     const [kwd, setKwd] = useState("");
 
+    const { userInfo } = useContext(UserContext);
+    const { setPostList } = useContext(PostListContext);
+    const { pageNumber } = useContext(PageLinkContext);
+    // ポスト一覧を取得する関数
+    const getSearchPostList = async() => {
+        const posts = await getList(userInfo.token, pageNumber);
+        // getListで取得したポスト配列をコンテキストに保存する
+        let postList: Array<PostType> = [];
+        if (posts) {
+        posts.forEach((p: any) => {
+            if (p.content.includes(kwd))
+            postList.push({
+            id: p.id,
+            user_name: p.user_name,
+            content: p.content,
+            created_at: new Date(p.created_at),
+            });
+        });
+        }
+        setPostList(postList);
+    }
+
     const onSearchPostClick = () => {
         console.log("kwd:", kwd)
+        getSearchPostList()
     }
 
     return (
         // <SSearchForm>
             <SSearchDiv>
-                <SSearchPost type="search" placeholder="検索" name="q" onChange={(evt) => setKwd(evt.target.value)}/>
+                <SSearchPost type="search" placeholder="検索" name="kwd" onChange={(evt) => setKwd(evt.target.value)}/>
                 <SSearchIcon type="submit" onClick={onSearchPostClick}><img src="https://github.com/MoeMiyata/Rank2-MessageBoard-Frontend/blob/main/public/searchicon.png?raw=true" alt="search" width="30" height="30" /></SSearchIcon>
             </SSearchDiv> 
         // </SSearchForm>
