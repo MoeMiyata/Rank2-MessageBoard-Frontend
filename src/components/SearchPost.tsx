0581a54a61492935c9e9c86594420fdbbcca055e
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-20 15:43:08 +0900

    メッセージ検索（検索ボタン設置）

--- src/components/SearchPost.tsx
+++ src/components/SearchPost.tsx
@@ -5,7 +5,7 @@ export const SearchPost = () => {
     return (
         <SSearchDiv>
             <SSearchPost type="search" placeholder="検索"/>
-            {/* <SSearchIcon type="submit"><img src="https://github.com/MoeMiyata/Rank2-MessageBoard-Frontend/blob/main/public/searchicon.png?raw=true" alt="search" width="30" height="30" /></SSearchIcon> */}
+            <SSearchIcon type="submit"><img src="https://github.com/MoeMiyata/Rank2-MessageBoard-Frontend/blob/main/public/searchicon.png?raw=true" alt="search" width="30" height="30" /></SSearchIcon>
         </SSearchDiv> 
     )
 }
@@ -23,6 +23,16 @@ const SSearchPost = styled.input`
   width: 400px;
 `
 
-const SSearchIcon = styled.input`
+const SSearchIcon = styled.button`
   filter: hue-rotate(180deg);
 `
+
+// const SPageLinkNextButton = styled.button`
+//   background-color: #222222;
+//   color: #FAFAFA;
+//   position: fixed;
+//   top: 45px;
+//   right: 10px;
+//   padding: 3px 5px 0px 4px;
+//   border-radius: 50%;
+// `
