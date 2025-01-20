1c741a313c1df022aff33a41106e6261cc5e6304
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-20 14:05:32 +0900

    メッセージ検索（検索ボタン設置）

--- src/components/SearchPost.tsx
+++ src/components/SearchPost.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 export const SearchPost = () => {
-    return <SSearchPost type="search" placeholder="検索"></SSearchPost>
+    return (
+        <SSearchPostDiv>
+            <SSearchPost type="search" placeholder="検索"/>
+        </SSearchPostDiv>
+    )
 }
+
+const SSearchPostDiv = styled.div`
+  
+`
 
 const SSearchPost = styled.input`
   border-radius: 4px;
   box-shadow: inset 0 2px 4px #CCCCCC;
+  margin: 2.5px;
+  text-align: center;
 `
