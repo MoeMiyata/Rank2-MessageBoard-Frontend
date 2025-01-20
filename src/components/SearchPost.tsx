aaa5d25a3532509898c2c3880986a6bd3b1051f0
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-20 14:23:22 +0900

    メッセージ検索（検索ボタンの位置修正）

--- src/components/SearchPost.tsx
+++ src/components/SearchPost.tsx
@@ -3,19 +3,14 @@ import styled from "styled-components";
 
 export const SearchPost = () => {
     return (
-        <SSearchPostDiv>
-            <SSearchPost type="search" placeholder="検索"/>
-        </SSearchPostDiv>
+        <SSearchPost type="search" placeholder="検索"/>
     )
 }
-
-const SSearchPostDiv = styled.div`
-  
-`
 
 const SSearchPost = styled.input`
   border-radius: 4px;
   box-shadow: inset 0 2px 4px #CCCCCC;
   margin: 2.5px;
   text-align: center;
+  width: 400px;
 `
