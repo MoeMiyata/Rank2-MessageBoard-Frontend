83c70b54283a057ef1217c7271a46e4975a15e0b
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-01-20 13:29:35 +0900

    メッセージの削除（削除後にリロードするよう修正）

--- src/components/DeletePost.tsx
+++ src/components/DeletePost.tsx
@@ -3,18 +3,38 @@ import styled from "styled-components"
 
 import { UserContext } from "../providers/UserProvider.tsx";
 import { deletePost, getList } from "../api/Post.tsx";
-import { ReloadPage } from "./ReloadPage.tsx";
+import { PostListContext, PostType } from "../providers/PostListProvider.tsx";
+import { PageLinkContext } from "../providers/PageLinkProvider.tsx";
 
 
 export const DeletePost = (props: any) => {
     const { deleteid } = props;
 
     const { userInfo } = useContext(UserContext);
+    const { setPostList } = useContext(PostListContext);
+    const { pageNumber } = useContext(PageLinkContext);
+    // ポスト一覧を取得する関数
+    const getPostList = async() => {
+        const posts = await getList(userInfo.token, pageNumber);
+        // getListで取得したポスト配列をコンテキストに保存する
+        let postList: Array<PostType> = [];
+        if (posts) {
+        posts.forEach((p: any) => {
+            postList.push({
+            id: p.id,
+            user_name: p.user_name,
+            content: p.content,
+            created_at: new Date(p.created_at),
+            });
+        });
+        }
+        setPostList(postList);
+    }
 
     // 対象のポストのみ削除してリロードする関数
     const deletePostReload = async() => {
         await deletePost(userInfo.token, deleteid);
-        ReloadPage()
+        getPostList()
     }
 
     const onDeletePostClick = () => {
