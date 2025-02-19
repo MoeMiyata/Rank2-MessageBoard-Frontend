699548782e7ff772f9dfb72235bee906930870ba
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-02-19 13:40:00 +0900

    ユーザ登録（signuplayout修正）

--- src/components/SignUpLayout.tsx
+++ src/components/SignUpLayout.tsx
@@ -14,6 +14,60 @@ export default function SignInLayout() {
     <>
     <h1>ユーザ登録画面</h1>
 
+    <SSignInFrame>
+      <SSignInRow>
+        <SSignInLabel>
+          <label htmlFor="name">Name</label>
+        </SSignInLabel>
+        
+        <SSignInInput>
+          <input
+            id="name"
+            // value={userId}
+            type="text"
+            // onChange={(evt) => setUserId(evt.target.value)}
+          />
+        </SSignInInput>
+      </SSignInRow>
+
+      <SSignInRow>
+        <SSignInLabel>
+          <label htmlFor="password">Password</label>
+        </SSignInLabel>
+
+        <SSignInInput>
+          <input
+            id="password"
+            // value={pass}
+            type="password"
+            // onChange={(evt) => setPass(evt.target.value)}
+          />
+        </SSignInInput>
+      </SSignInRow>
+
+      <SSignInRow>
+        <SSignInLabel>
+          <label htmlFor="email">Email</label>
+        </SSignInLabel>
+
+        <SSignInInput>
+          <input
+            id="email"
+            // value={pass}
+            type="email"
+            // onChange={(evt) => setPass(evt.target.value)}
+          />
+        </SSignInInput>
+      </SSignInRow>
+
+      <SSignInRow>
+        {/* <SLoginButton type="button" onClick={onSignInClick}> */}
+        <SLoginButton type="button">
+          Create New Account
+        </SLoginButton>
+      </SSignInRow>
+    </SSignInFrame>
+
     <SSignInRow>
     <SLoginButton type="button" onClick={onSignUpClick}>
         Back to Login
@@ -23,10 +77,34 @@ export default function SignInLayout() {
   );
 }
 
+const SSignInFrame = styled.div`
+  background-color: #f8f8f8;
+  margin: 80px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  border-radius: 8px;
+  box-shadow: 0 8px 8px #aaaaaa;
+`;
+
 const SSignInRow = styled.div`
   dixplay: inline-block;
   margin-top: 4px;
   margin-bottom: 4px;
+`;
+
+const SSignInLabel = styled.span`
+  display: inline-block;
+  width: 25%;
+  vertical-align: top;
+  text-align: right;
+  margin-right: 4px;
+`;
+
+const SSignInInput = styled.span`
+  display: inline-block;
+  width: auto;
+  vertical-align: top;
+  margin-left: 4px;
 `;
 
 const SLoginButton = styled.button`
