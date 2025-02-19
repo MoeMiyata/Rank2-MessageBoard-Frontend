c1361fc5ffff697706f7b9c3beb976b4eeff1b3e
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-02-19 13:24:03 +0900

    ユーザ登録（signupページにloginページに戻るボタン設置）

--- src/components/SignUpLayout.tsx
+++ src/components/SignUpLayout.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 
 export default function SignInLayout() {
+  const navigate = useNavigate();
+
+  const onSignUpClick = async () => {
+    navigate("/");
+  };
+      
   return (
+    <>
     <h1>ユーザ登録画面</h1>
+
+    <SSignInRow>
+    <SLoginButton type="button" onClick={onSignUpClick}>
+        Back to Login
+    </SLoginButton>
+    </SSignInRow>
+    </>
   );
 }
+
+const SSignInRow = styled.div`
+  dixplay: inline-block;
+  margin-top: 4px;
+  margin-bottom: 4px;
+`;
+
+const SLoginButton = styled.button`
+  background-color: #444444;
+  color: #f0f0f0;
+  padding: 4px 16px;
+  border-radius: 8px;
+`;
