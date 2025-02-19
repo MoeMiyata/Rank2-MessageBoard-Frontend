52fba74c27097dd8d834a3597279f529ea67bbdd
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-02-19 11:52:55 +0900

    メッセージ検索（frontendからbackend機能に変更、SearchProviderの位置変更）

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { UserContext } from "../providers/UserProvider.tsx";
 import { getUser } from "../api/User.tsx";
 import { SearchPost } from './SearchPost.tsx';
+import { SearchPostProvider } from "../providers/SearchPostProvider.tsx";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -25,6 +26,8 @@ export default function Header() {
   }, []);
 
   return (
+    <SearchPostProvider>
+
     <SHeader>
       <SLogo>MicroPost</SLogo>
 
@@ -36,6 +39,8 @@ export default function Header() {
         <SLogout onClick={logout}>ログアウト</SLogout>
       </SRightItem>
     </SHeader>
+    
+    </SearchPostProvider>
   )
 }
 
