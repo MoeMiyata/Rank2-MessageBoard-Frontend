889b2f6c7c37a4e1974ac55b2d4d860828ca5b63
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-03-21 13:38:38 +0900

    編集内容をbackendにも反映

--- src/api/User.tsx
+++ src/api/User.tsx
@@ -34,6 +34,7 @@ export const createUser = async (name: string, email: string, password: string) 
 export const getUser = async (user_id: number, token: string) => {
   // const url = `http://localhost:3000/user/${user_id}?token=${token}`;
   const url = hostUrl + `/user/${user_id}?token=${token}`;
+  console.log(url)
   const res = await axios.get(url);
 
   console.log('res(getUser):', res.data);
