f2a07834cfd8cae7853c0e6fe575fd2732b8d9de
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-04-28 17:35:03 +0900

    judgeOfNextPage を呼び出すタイミングを調整

--- src/components/PageLink.tsx
+++ src/components/PageLink.tsx
@@ -1,3 +1,86 @@
+// import React, { useContext, useEffect, useState } from 'react';
+// import styled from 'styled-components';
+// import { PageLinkContext } from '../providers/PageLinkProvider.tsx';
+// import { UserContext } from '../providers/UserProvider.tsx';
+// import { getList } from '../api/Post.tsx';
+
+// export default function PageLink() {
+//     const { userInfo } = useContext(UserContext);
+//     const { pageNumber } = useContext(PageLinkContext);
+//     const { setPageNumber } = useContext(PageLinkContext);
+
+//     const [isExistNextPage, setIsExistNextPage] = useState<boolean>(false); // 次ページがあるかどうかの状態管理
+
+//     // const judgeOfNextPage = async () => {
+//     //     const posts = await getList(userInfo.token, pageNumber+10); // +20にしているのはstateの更新とレンダリングのタイミングが合わないので帳尻合わせで10->20に変更  -> 合わなかったのでまた10に戻した
+//     //     console.log("judgeOfNextPage(posts):", posts);
+//     //     setIsExistNextPage(posts.length > 0);
+//     // }
+//     const judgeOfNextPage = async () => {
+//       const nextPage = pageNumber + 10;
+//       const posts = await getList(userInfo.token, nextPage);
+//       console.log("judgeOfNextPage(posts):", posts);
+//       setIsExistNextPage(Array.isArray(posts) && posts.length > 0);
+//   }
+
+
+//     const onBeforePageClick = async () => {
+//         // await setPageNumber((prevPage) => prevPage -= 10);
+//         setPageNumber((prevPage) => prevPage -= 10);
+//     }
+
+//     const onNextPageClick = async () => {
+//         // await setPageNumber((prevPage) => prevPage += 10);
+//         setPageNumber((prevPage) => prevPage += 10);
+//     }
+
+
+//     useEffect(() => {
+//         judgeOfNextPage();
+//     }, [pageNumber]); 
+    
+
+// 	return (
+//         <SPageLink>
+//             <SPageLinkRow>
+//                 { !(pageNumber<10) ? <SPageLinkBeforeButton onClick={onBeforePageClick}>前へ</SPageLinkBeforeButton> : null }
+//                 { (isExistNextPage) && <SPageLinkNextButton onClick={onNextPageClick}>次へ</SPageLinkNextButton> }
+//                 {/* { ((pageNumber<10)||isExistNextPage) && <SPageLinkNextButton onClick={onNextPageClick}>次へ</SPageLinkNextButton> } */}
+//             </SPageLinkRow>
+//         </SPageLink>
+// 	)
+// }
+
+
+// const SPageLink = styled.div`
+//   padding: 8px;
+//   margin-bottom: 8px;
+// `
+
+// const SPageLinkRow = styled.div`
+//   margin-top: 4px;
+//   margin-bottom: 4px;
+//   text-align: rigth;
+// `
+
+// const SPageLinkBeforeButton = styled.button`
+//   background-color: #222222;
+//   padding: 4px;
+//   border-radius: 8px;
+//   color: #FAFAFA;
+//   width: 100%;
+//   width: 15%;
+// `
+
+// const SPageLinkNextButton = styled.button`
+//   background-color: #222222;
+//   padding: 4px;
+//   border-radius: 8px;
+//   color: #FAFAFA;
+//   width: 100%;
+//   width: 15%;
+// `
+
 import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { PageLinkContext } from '../providers/PageLinkProvider.tsx';
@@ -11,57 +94,46 @@ export default function PageLink() {
 
     const [isExistNextPage, setIsExistNextPage] = useState<boolean>(false); // 次ページがあるかどうかの状態管理
 
-    // const judgeOfNextPage = async () => {
-    //     const posts = await getList(userInfo.token, pageNumber+10); // +20にしているのはstateの更新とレンダリングのタイミングが合わないので帳尻合わせで10->20に変更  -> 合わなかったのでまた10に戻した
-    //     console.log("judgeOfNextPage(posts):", posts);
-    //     setIsExistNextPage(posts.length > 0);
-    // }
-    const judgeOfNextPage = async () => {
-      const nextPage = pageNumber + 10;
-      const posts = await getList(userInfo.token, nextPage);
+    const judgeOfNextPage = async (page: number) => {
+      const posts = await getList(userInfo.token, page);
       console.log("judgeOfNextPage(posts):", posts);
       setIsExistNextPage(Array.isArray(posts) && posts.length > 0);
-  }
+    }
 
-
+    // ページ遷移前に次ページの判定を行う
     const onBeforePageClick = async () => {
-        // await setPageNumber((prevPage) => prevPage -= 10);
-        setPageNumber((prevPage) => prevPage -= 10);
+        setPageNumber((prevPage) => prevPage - 10);
     }
 
     const onNextPageClick = async () => {
-        // await setPageNumber((prevPage) => prevPage += 10);
-        setPageNumber((prevPage) => prevPage += 10);
+        setPageNumber((prevPage) => prevPage + 10);
     }
 
-
+    // `pageNumber`が変更されるたびに次ページが存在するかを再判定
     useEffect(() => {
-        judgeOfNextPage();
-    }, [pageNumber]); 
-    
+        judgeOfNextPage(pageNumber + 10);  // 次ページを判定するために現在のpageNumber+10を渡す
+    }, [pageNumber]);
 
-	return (
+    return (
         <SPageLink>
             <SPageLinkRow>
-                { !(pageNumber<10) ? <SPageLinkBeforeButton onClick={onBeforePageClick}>前へ</SPageLinkBeforeButton> : null }
-                { (isExistNextPage) && <SPageLinkNextButton onClick={onNextPageClick}>次へ</SPageLinkNextButton> }
-                {/* { ((pageNumber<10)||isExistNextPage) && <SPageLinkNextButton onClick={onNextPageClick}>次へ</SPageLinkNextButton> } */}
+                { !(pageNumber < 10) && <SPageLinkBeforeButton onClick={onBeforePageClick}>前へ</SPageLinkBeforeButton> }
+                { isExistNextPage && <SPageLinkNextButton onClick={onNextPageClick}>次へ</SPageLinkNextButton> }
             </SPageLinkRow>
         </SPageLink>
-	)
+    )
 }
-
 
 const SPageLink = styled.div`
   padding: 8px;
   margin-bottom: 8px;
-`
+`;
 
 const SPageLinkRow = styled.div`
   margin-top: 4px;
   margin-bottom: 4px;
-  text-align: rigth;
-`
+  text-align: right;
+`;
 
 const SPageLinkBeforeButton = styled.button`
   background-color: #222222;
@@ -70,7 +142,7 @@ const SPageLinkBeforeButton = styled.button`
   color: #FAFAFA;
   width: 100%;
   width: 15%;
-`
+`;
 
 const SPageLinkNextButton = styled.button`
   background-color: #222222;
@@ -79,4 +151,4 @@ const SPageLinkNextButton = styled.button`
   color: #FAFAFA;
   width: 100%;
   width: 15%;
-`
+`;
