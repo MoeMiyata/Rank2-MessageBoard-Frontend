118a02d34c6875ceb627ad8f7b44f7fa22822f23
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-05-08 13:31:03 +0900

    DeleteDialogレイアウト修正

--- src/components/DeleteUser.tsx
+++ src/components/DeleteUser.tsx
@@ -34,12 +34,14 @@ export const DeleteUser = () => {
             Delete account
         </SDeleteUserButton>
         { isOpenDialog && (
-          <SDeleteDialog>
-            <p style={{ marginBottom: 0 }}>アカウントを削除しますか？</p>
-            <p style={{ marginTop: 0 , fontSize: "small"}}>※削除後この操作は取り消せません。</p>
-            <SDeleteDialogButton onClick={onDeleteUserClick}>削除</SDeleteDialogButton>
-            <SDeleteDialogButton onClick={onClickSwitchDialog}>キャンセル</SDeleteDialogButton>
-          </SDeleteDialog>
+          <SDeleteDialogContainer>
+            <SDeleteDialog>
+              <p style={{ marginBottom: 0 }}>アカウントを削除しますか？</p>
+              <p style={{ marginTop: 0 , fontSize: "small"}}>※削除後この操作は取り消せません。</p>
+              <SDeleteDialogButton buttonText="削除" onClick={onDeleteUserClick}>削除</SDeleteDialogButton>
+              <SDeleteDialogButton buttonText="キャンセル" onClick={onClickSwitchDialog}>キャンセル</SDeleteDialogButton>
+            </SDeleteDialog>
+          </SDeleteDialogContainer>
           )}
       </>
     )
@@ -62,18 +64,25 @@ const SDeleteUserButton = styled.button`
   }
 `;
 
+const SDeleteDialogContainer = styled.div`
+  display: inline-block;
+  justify-content: center;
+`;
+
 const SDeleteDialog = styled.div`
   background-color: #f8f8f8;
+  width: 300px;
   padding-top: 8px;
   padding-bottom: 8px;
   border-radius: 8px;
   box-shadow: 0 8px 8px #aaaaaa;
 `;
 
-const SDeleteDialogButton = styled.button`
+const SDeleteDialogButton = styled.button<{ buttonText: string }>`
   border: none;
   border-radius: 5px;
   // background-color: lightblue;
   background-color:  #f8f8f8;
-  padding: 2px 10px;
+  // padding: 2px 10px;
+  padding: ${({ buttonText }) => buttonText === "削除" ? "2px 20px 2px 10px" : "2px 10px 2px 20px"};
 `;
