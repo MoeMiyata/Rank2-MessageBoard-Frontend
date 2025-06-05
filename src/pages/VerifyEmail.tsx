6f1ea901d5294cc131922fe9a20ff0609ffdc81b
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-06-05 10:25:04 +0900

    error/hostUrl: importファイル名変更

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import { hostUrl } from '../api/hostUrl';
+import { hostUrl } from '../api/hostUrl.ts';
 
 export const VerifyEmail = () => {
   const [ params ] = useSearchParams();
