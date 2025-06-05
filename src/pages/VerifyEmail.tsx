105cbf69ab85ed03fc439251f3f43b49319502da
MIYATA Moe <moe.miyata.1025@gmail.com>
2025-06-05 10:04:47 +0900

    feature/maillink: メール認証ページ実装

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,17 +1,21 @@
 import { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
+import { hostUrl } from '../api/hostUrl';
 
 export const VerifyEmail = () => {
-  const [params] = useSearchParams();
+  const [ params ] = useSearchParams();
   const token = params.get('token');
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (token) {
       axios
-        .post('https://rank2-messageboard-backend.onrender.com/user/verify-email', { token })
+        .post(hostUrl + '/user/verify-email', { token })
         .then((res) => {
           alert('✅ メール認証が完了しました！');
+          navigate('/main');
         })
         .catch((err) => {
           alert('❌ 認証に失敗しました');
